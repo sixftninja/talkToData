d9e10fa2c172d637a5b62b5bb870db6e893b9d24
sixftninja <anandaltekar@gmail.com>
2021-06-17 15:20:15 -0400

    Add files via upload

---
+++ jsonl.py
@@ -0,0 +1,30 @@
+import csv
+import jsonlines
+import json
+import pandas as pd
+from tabulate import tabulate
+
+def get_data_from_excel():
+    xls = pd.read_excel('test_data.xlsx')
+    data = xls.to_dict(orient='records')
+    return data
+
+def write_json_lines(data):
+    with open('testData1.JSONL', 'wb') as f:
+        writer = jsonlines.Writer(f)
+        for line in data:
+            line = {"text": line}
+            writer.write(line)
+
+def main():
+    raw_data = get_data_from_excel()
+    # print(data)
+    dataToJSONL = []
+    for line in raw_data:
+        line = str(line).strip("{}").replace("'", "")
+        dataToJSONL.append(line)
+    write_json_lines(dataToJSONL)
+    print("jsonL written..")
+
+if __name__ == "__main__":
+    main() 
---
+++ talkToData.py
@@ -0,0 +1,86 @@
+
+from logging import PlaceHolder
+import os
+import requests
+from typing import get_type_hints
+import openai
+import pandas as pd
+import numpy as np
+import streamlit as st
+from information import *
+import json
+
+def main():
+  st.sidebar.title("Talk to your data!")
+  # st.sidebar.header("Controls")
+
+  api_key = os.getenv('OPENAI_API_KEY')
+
+  df1 = pd.read_excel("test_data.xlsx")
+  df2 = pd.read_excel("test_data2.xlsx")
+
+  # excelFiles = [df1, df2]
+
+  st.sidebar.header("Choose data:")
+  fileSelection = st.sidebar.selectbox(
+    "Which file would you like to use?",
+    ['Machine 1 Data','Machine 2 Data']
+  )
+
+  if fileSelection == 'Machine 1 Data':
+    st.dataframe(df1)
+    fileID = machine1_fileID
+    gpt_examples_context = gpt_examples_context_1
+    st_examples_context = st_examples_context_1
+    gpt_example_qa = gpt_example_qa_1
+    st_example_qa = st_example_qa_1
+  else:
+    st.dataframe(df2)
+    fileID = machine2_fileID
+    gpt_examples_context = gpt_examples_context_2
+    st_examples_context = st_examples_context_2
+    gpt_example_qa = gpt_example_qa_2
+    st_example_qa = st_example_qa_2
+
+  file = fileID
+  max_tokens = 10
+  st.sidebar.subheader('Temperature')
+  st_temperature = st.sidebar.slider(
+    '',min_value=0.0, max_value=1.0, step=0.05, value=0.5
+    )
+  stop=["\n", "<|endoftext|>"]
+
+  search_model = st.sidebar.selectbox(
+    "Select search model:",
+    ['davinci', 'curie', 'babbage', 'ada']
+  )
+  answer_model = st.sidebar.selectbox(
+    "Select answer model:",
+    ['curie', 'davinci', 'babbage', 'ada']
+  )
+  with st.form(key='my form'):
+    mainQuestion = st.text_input(label="Enter your question", value="What was the output of the machine at 10 a.m.?", max_chars=128)
+    submit_question = st.form_submit_button("Submit")
+  displayAnswer = st.empty()
+  displaydocument = st.empty()
+  if submit_question:
+    gptAnswer = openai.Answer.create(
+        search_model=search_model,
+        model=answer_model,
+        file = fileID,
+        examples_context = gpt_examples_context,
+        examples = gpt_example_qa,
+        max_tokens=max_tokens,
+        question = mainQuestion,
+        temperature = st_temperature
+        )
+    # show progress
+    # display answer
+    displayAnswer.write(gptAnswer['answers'][0])
+  st.subheader("Examples' Context:")
+  st.text(st_examples_context)
+  st.subheader("Examples:")
+  st.text(st_example_qa)
+
+if __name__ == "__main__":
+    main()
